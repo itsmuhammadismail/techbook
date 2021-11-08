eabac508866ace92ccc8ad88f91b162d6b189ac0
Abdul Rehman/IT/HO <AbdulRehman@tcs.com.pk>
2021-11-08 14:54:07 +0500

    abdulrehman

--- components/Team/Leads.js
+++ components/Team/Leads.js
@@ -21,7 +21,11 @@ const Leads = () => {
             <a>
               <div className="flex flex-col items-center w-[16rem]  h-[19rem]  ">
                 <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
-                  <img src="/Lead.svg" alt="" className="h-[100%]  mb-[-0.1rem]" />
+                  <img
+                    src="/Lead.svg"
+                    alt=""
+                    className="h-[100%]  mb-[-0.1rem]"
+                  />
                 </div>
                 <h2 className="text-xl mt-4 ">Rumman Hussain Siddiqui</h2>
                 <p className="text-center text-sm">Senior Android Developer</p>
@@ -113,6 +117,93 @@ const Leads = () => {
                 <h2 className="text-xl mt-4 ">Shahbaz Ali</h2>
                 <p className="text-center text-sm">
                   AM Middleware & Integrations
+                </p>
+              </div>
+            </a>
+          </Link>
+        </Zoom>
+        <Zoom delay={1000}>
+          <Link href="/javed-khan">
+            <a>
+              <div className="flex flex-col items-center w-[16rem]  h-[19rem]  ">
+                <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
+                  <img src="/Lead.svg" alt="" className="h-[100%]" />
+                </div>
+                <h2 className="text-xl mt-4 ">Javed A Khan</h2>
+                <p className="text-center text-sm">Senior Manager ETL & BI</p>
+              </div>
+            </a>
+          </Link>
+        </Zoom>
+        <Zoom delay={1500}>
+          <Link href="/m-farrukh">
+            <a>
+              <div className="flex flex-col items-center w-[16rem] h-[19rem]   ">
+                <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
+                  <img
+                    src="/Lead.svg"
+                    alt=""
+                    className="h-[100%] mb-[-0.5rem]"
+                  />
+                </div>
+                <h2 className="text-xl mt-4 ">Muhammad Farrukh</h2>
+                <p className="text-center text-sm">
+                  Senior Database - Administrator
+                </p>
+              </div>
+            </a>
+          </Link>
+        </Zoom>
+        <Zoom delay={2000}>
+          <Link href="/anwar-ali">
+            <a>
+              <div className="flex flex-col items-center w-[16rem] h-[19rem]   ">
+                <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
+                  <img
+                    src="/Lead.svg"
+                    alt=""
+                    className="h-[100%] mb-[-0.5rem]"
+                  />
+                </div>
+                <h2 className="text-xl mt-4 ">Anwar Ali</h2>
+                <p className="text-center text-sm">System Analyst</p>
+              </div>
+            </a>
+          </Link>
+        </Zoom>
+        <Zoom delay={2500}>
+          <Link href="/tausif-akbar">
+            <a>
+              <div className="flex flex-col items-center w-[16rem] h-[19rem]   ">
+                <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
+                  <img
+                    src="/Lead.svg"
+                    alt=""
+                    className="h-[100%] mb-[-0.5rem]"
+                  />
+                </div>
+                <h2 className="text-xl mt-4 ">Tausif Akbar Jilani</h2>
+                <p className="text-center text-sm">
+                  Heqd Financial Software System
+                </p>
+              </div>
+            </a>
+          </Link>
+        </Zoom>
+        <Zoom delay={3000}>
+          <Link href="/talha-ishaq">
+            <a>
+              <div className="flex flex-col items-center w-[16rem] h-[19rem]   ">
+                <div className="blueImage rounded-lg w-full h-[13rem] hover:w-[110%] hover:h-[14rem] pt-[2rem] flex justify-center items-center">
+                  <img
+                    src="/Lead.svg"
+                    alt=""
+                    className="h-[100%] mb-[-0.5rem]"
+                  />
+                </div>
+                <h2 className="text-xl mt-4 ">Talha Ishaq</h2>
+                <p className="text-center text-sm">
+                  Manager Development Software Engineering
                 </p>
               </div>
             </a>
