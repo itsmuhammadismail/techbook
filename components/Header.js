70568c4c011047b27d5a7f0515e5726ca7176c59
Muhammad Ismail <ismail.muhammad@tcs.com.pk>
2021-12-13 12:47:57 +0500

    ismail

--- components/Header.js
+++ components/Header.js
@@ -10,33 +10,35 @@ const Header = () => {
     setMenu(true);
   };
   return (
-    <header
-      id="top"
-      className="media mx-auto flex justify-between items-center p-[1.5rem]"
-    >
-      <div className="">
-        <Link href="/">
-          <a>
-            <img
-              src="/home/logo.svg"
-              alt=""
-              className="h-[1.5rem] sm:h-[1.8rem] 2xl:h-[2rem]"
-            />
-          </a>
-        </Link>
-      </div>
-      <div className="hidden md:block ">
-        <Nav />
-      </div>
-      <img
-        className="block md:hidden"
-        src="/menu.svg"
-        onClick={showMenuDrawer}
-      />
+    <div className="sticky top-0 w-[100%] z-[1000000] bg-white">
+      <header
+        id="top"
+        className="media mx-auto flex justify-between items-center p-[1.5rem] "
+      >
+        <div className="">
+          <Link href="/">
+            <a>
+              <img
+                src="/home/logo.svg"
+                alt=""
+                className="h-[1.5rem] sm:h-[1.8rem] 2xl:h-[2rem]"
+              />
+            </a>
+          </Link>
+        </div>
+        <div className="hidden md:block ">
+          <Nav />
+        </div>
+        <img
+          className="block md:hidden"
+          src="/menu.svg"
+          onClick={showMenuDrawer}
+        />
 
-      {/* Drawers */}
-      <MenuDrawer visible={menu} setVisible={setMenu} />
-    </header>
+        {/* Drawers */}
+        <MenuDrawer visible={menu} setVisible={setMenu} />
+      </header>
+    </div>
   );
 };
 
